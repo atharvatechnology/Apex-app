19d80a0960b03b48e0278f5cd808be8bd185efdd
rojesh mote <rojesh123mote@gmail.com>
2022-06-28 15:27:49 +0545

    page scss and nevigate are corrected

--- src/components/modules/signin/Login.js
+++ src/components/modules/signin/Login.js
@@ -5,66 +5,64 @@
 
 */
 
+import React from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
 
-import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
-
-import CustomTextInput from "@elements/CustomTextInput";
-import CustomButton from "@elements/CustomButton";
-import Header from "@elements/Header";
+import CustomTextInput from '@elements/CustomTextInput';
+import CustomButton from '@elements/CustomButton';
+import Header from '@elements/Header';
 import styles from '@styles/modules/signin/login.scss';
 
+const Login = props => {
+  const handleSignupLink = () => {
+    props.navigation.navigate('Register');
+  };
 
+  const handleResetLink = () => {
+    props.navigation.navigate('Reset');
+  };
+  const handleSigninPress = () => {
+    props.navigation.navigate('Login');
+  };
 
-const Login = (props) => {
+  return (
+    <View style={styles.container}>
+      <Header />
 
-    const handleSignupLink = () => {
-        props.navigation.navigate('Register')
-    }
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}> Login</Text>
 
-    const handleResetLink = () => {
-        props.navigation.navigate('Reset')
-    }
-    const handleSigninPress = () => {
-        props.navigation.navigate('Login')
-    }
-
-    return (
-        <View style={styles.container}>
-            <Header />
-
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}> Login</Text>
-
-                <View style={styles.p}>
-                    <Text style={styles.p}> Haven't got an account?</Text>
-                    <TouchableOpacity onPress={handleSignupLink} >
-                        <Text style={styles.link} >  Sign up.</Text>
-
-                    </TouchableOpacity>
-                </View>
-
-            </View>
-
-            <View style={styles.formContainer}>
-                <CustomTextInput onChange={() => { }} placeholder="Phone Number" />
-                <CustomTextInput onChange={() => { }} placeholder="Password" hidden={true} />
-            </View>
-            <View style={styles.lastcontainer}>
-                <Text style={styles.p}>Forget password?</Text>
-                <TouchableOpacity onPress={handleResetLink} >
-                    <Text style={styles.link1} > Reset.</Text>
-                </TouchableOpacity>
-            </View>
-
-            <CustomButton
-                type='theme'
-                title={'Sign in'}
-                style={styles.signIn}
-                onPress={handleSigninPress}
-            />
+        <View style={styles.p}>
+          <Text style={styles.p}> Haven't got an account?</Text>
+          <TouchableOpacity onPress={handleSignupLink}>
+            <Text style={styles.link}> Sign up.</Text>
+          </TouchableOpacity>
         </View>
-    );
-}
+      </View>
+
+      <View style={styles.formContainer}>
+        <CustomTextInput onChange={() => {}} placeholder="Phone Number" />
+        <CustomTextInput
+          onChange={() => {}}
+          placeholder="Password"
+          hidden={true}
+        />
+      </View>
+      <View style={styles.lastcontainer}>
+        <Text style={styles.p}>Forget password?</Text>
+        <TouchableOpacity onPress={handleResetLink}>
+          <Text style={styles.link1}> Reset.</Text>
+        </TouchableOpacity>
+      </View>
+
+      <CustomButton
+        type="theme"
+        title={'Sign in'}
+        style={styles.signIn}
+        onPress={handleSigninPress}
+      />
+    </View>
+  );
+};
 
 export default Login;
--- src/components/modules/walkthrough/walkthrough.js
+++ src/components/modules/walkthrough/walkthrough.js
@@ -1,20 +1,17 @@
 /**
-* This is waklthrough page containing the logo of the organization. This page automatically redirects to on boarding, sign in or dashboard after 4 seconds.
-* @param {Object} props.navigation - contains all the propeties of react navigation
-* @returns {Walkthrough}- returns a module for walkthrough
-*/
+ * This is waklthrough page containing the logo of the organization. This page automatically redirects to on boarding, sign in or dashboard after 4 seconds.
+ * @param {Object} props.navigation - contains all the propeties of react navigation
+ * @returns {Walkthrough}- returns a module for walkthrough
+ */
 
-
-import React, { useEffect } from "react";
-import { Image, Text, View, Animated } from "react-native";
+import React, {useEffect} from 'react';
+import {Image, Text, View, Animated} from 'react-native';
 
 // import Animated, { FadeIn } from 'react-native-reanimated';
 
 import styles from '@styles/modules/walkthrough.scss';
 
-
-const Walkthrough = (props) => {
-
+const Walkthrough = props => {
   const startValue = new Animated.Value(1);
   const endValue = 1.5;
 
@@ -38,7 +35,6 @@ const Walkthrough = (props) => {
 
   return (
     <View style={styles.container}>
-
       {/* <Animated.View entering={FadeIn.delay(500).duration(700)} style={styles.test}> */}
       <Animated.Image
         style={[
@@ -53,9 +49,8 @@ const Walkthrough = (props) => {
         source={require('@assets/images/apexLogo.png')}
       />
       {/* </Animated.View> */}
-
     </View>
   );
-}
+};
 
 export default Walkthrough;
