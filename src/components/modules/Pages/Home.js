fd3abf689286e7df1f0717f77ad8520393771504
rojesh mote <rojesh123mote@gmail.com>
2022-07-06 15:04:23 +0545

    Home page completed

--- src/components/modules/Pages/Home.js
+++ src/components/modules/Pages/Home.js
@@ -5,7 +5,7 @@
  */
 
 import React, {useState, useRef} from 'react';
-import {Dimensions, Text, View, ScrollView} from 'react-native';
+import {Dimensions, Text, View, ScrollView, Image} from 'react-native';
 
 import Carousel, {Pagination} from 'react-native-snap-carousel';
 
@@ -84,29 +84,81 @@ const data = [
     amount: `Rs.500 \u25CF 60 min`,
   },
 ];
+const data2 = [
+  {
+    image: '',
+    main: 'IOM',
+    main1: 'Multiple Section',
+    info: 'Medical Entrance (ME-CEE) with multiple line ',
+    date: 'Starting on Feb ,2022 (4 month)',
+    data: ' 200+ students enrolled',
+  },
+  {
+    image: '',
+    main: 'IOM',
+    main1: 'Multiple Section',
+    info: 'Medical Entrance (ME-CEE) with multiple line',
+    date: 'Starting on Feb ,2022 (4 month)',
+    data: ' 200+ students enrolled',
+  },
+  {
+    image: '',
+    main: 'IOM',
+    main1: 'Multiple Section',
+    info: 'Medical Entrance (ME-CEE) with multiple line',
+    date: 'Starting on Feb ,2022 (4 month)',
+    data: ' 200+ students enrolled',
+  },
+  {
+    image: '',
+    main: 'IOM',
+    main1: 'Multiple Section',
+    info: 'Medical Entrance (ME-CEE) with multiple line',
+    date: 'Starting on Feb ,2022 (4 month)',
+    data: ' 200+ students enrolled',
+  },
+  {
+    image: '',
+    main: 'IOM',
+    main1: 'Multiple Section',
+    info: 'Medical Entrance (ME-CEE) with multiple line ',
+    date: 'Starting on Feb ,2022 (4 month)',
+    data: ' 200+ students enrolled',
+  },
+];
 
 const Home = props => {
   const [activeSlide, setActiveSlide] = useState(0);
 
   const [activeSlides, setActiveSlides] = useState(0);
 
+  const [activeSlidess, setActiveSlidess] = useState(0);
+
+  const [activeSlidesss, setActiveSlidesss] = useState(0);
+
   const CarouselRef = useRef(null);
 
   const CarouselReff = useRef(null);
 
+  const CarouselRefff = useRef(null);
+
+  const CarouselReffff = useRef(null);
+
   const _renderItemWithParallax = ({item, index}, parallaxProps) => {
     return (
       <>
-        <View style={styles.card}>
-          <View style={styles.file}></View>
-          <Text style={styles.title}>{item.title}</Text>
-          <Text style={styles.title1}>{item.title1}</Text>
-        </View>
+        <View style={styles.cards}>
+          <View style={styles.card}>
+            <View style={styles.file}></View>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.title1}>{item.title1}</Text>
+          </View>
 
-        <View>
-          <Text style={styles.text}>{item.text}</Text>
+          <View>
+            <Text style={styles.text}>{item.text}</Text>
 
-          <Text style={styles.amount}>{item.amount}</Text>
+            <Text style={styles.amount}>{item.amount}</Text>
+          </View>
         </View>
       </>
     );
@@ -114,16 +166,48 @@ const Home = props => {
   const _renderItemWithParallax1 = ({item, index}, parallaxProps) => {
     return (
       <>
-        <View style={styles.card}>
-          <View style={styles.file}></View>
+        <View style={styles.cards}>
+          <View style={styles.card}>
+            <View style={styles.file}></View>
 
-          <Text style={styles.title1}>{item.title1}</Text>
+            <Text style={styles.title1}>{item.title1}</Text>
+          </View>
+
+          <View>
+            <Text style={styles.text}>{item.text}</Text>
+
+            <Text style={styles.amount}>{item.amount}</Text>
+          </View>
         </View>
+      </>
+    );
+  };
+  const _renderItemWithParallax2 = ({item, index}, parallaxProps) => {
+    return (
+      <>
+        <View style={styles.cards}>
+          <View style={styles.img}>
+            <Image
+              style={styles.image}
+              source={require('@assets/images/home.jpeg')}
+            />
+          </View>
 
-        <View>
-          <Text style={styles.text}>{item.text}</Text>
+          <View style={styles.card}>
+            <Text style={styles.main}>{item.main}</Text>
 
-          <Text style={styles.amount}>{item.amount}</Text>
+            <Text style={styles.main1}>{item.main1}</Text>
+          </View>
+
+          <View>
+            <View>
+              <Text style={styles.info}>{item.info}</Text>
+            </View>
+
+            <Text style={styles.date}>{item.date}</Text>
+
+            <Text style={styles.data}>{item.data}</Text>
+          </View>
         </View>
       </>
     );
@@ -132,94 +216,178 @@ const Home = props => {
   return (
     <>
       <ScrollView style={styles.scrollView}>
-        <View styles={styles.navbar}>
-          <NavBar />
-        </View>
-        <View>
-          <View style={styles.txt}>
-            <Text style={styles.p}>Live exams</Text>
+        <View style={styles.division}>
+          <View styles={styles.navbar}>
+            <NavBar />
           </View>
-          <View style={styles.container}>
-            <View style={styles.textContainer}>
-              <Carousel
-                ref={CarouselRef}
-                data={data}
-                renderItem={_renderItemWithParallax}
-                sliderWidth={WIDTH}
-                itemWidth={WIDTH}
-                onSnapToItem={index => setActiveSlide(index)}
-              />
-
-              <View style={styles.paginationContainers}>
-                <Pagination
-                  dotsLength={data.length}
-                  activeDotIndex={activeSlide}
-                  containerStyle={styles.pagiStyle}
-                  dotColor={'#2E3192'}
-                  dotStyle={styles.pagiDot}
-                  inactiveDotColor={'white'}
-                  //  i changed collor
-                  inactiveDotOpacity={0.4}
-                  inactiveDotScale={1}
-                  inactiveDotStyle={styles.inactDotStyle}
-                  carouselRef={CarouselRef}
-                  tappableDots={!!CarouselRef}
-                />
-              </View>
-              <CustomButton
-                type="theme"
-                title={'Explore all'}
-                style={styles.button}
-                color="#000000"
-              />
-            </View>
-          </View>
-        </View>
-        <View>
-          <View style={styles.div}>
+          <View>
+            <View style={styles.gap} />
             <View style={styles.txt}>
-              <Text style={styles.p}>Practice exams</Text>
+              <Text style={styles.p}>Live exams</Text>
             </View>
-            <View style={styles.textContainer}>
-              <Carousel
-                ref={CarouselReff}
-                data={data1}
-                renderItem={_renderItemWithParallax1}
-                sliderWidth={WIDTH}
-                itemWidth={WIDTH}
-                onSnapToItem={index => setActiveSlides(index)}
-              />
-              <View style={styles.paginationContainers}>
-                <Pagination
-                  dotsLength={data.length}
-                  activeDotIndex={activeSlides}
-                  containerStyle={styles.pagiStyle}
-                  dotColor={'#2E3192'}
-                  dotStyle={styles.pagiDot}
-                  inactiveDotColor={'white'}
-                  inactiveDotOpacity={0.4}
-                  inactiveDotScale={1}
-                  inactiveDotStyle={[
-                    styles.inactDotStyle,
-                    {width: Dimensions.get('window').width * 0.15},
-                  ]}
-                  carouselRef={CarouselReff}
-                  tappableDots={!!CarouselReff}
+            <View style={styles.container}>
+              <View style={styles.textContainer}>
+                <Carousel
+                  ref={CarouselRef}
+                  data={data}
+                  renderItem={_renderItemWithParallax}
+                  sliderWidth={WIDTH}
+                  itemWidth={WIDTH}
+                  onSnapToItem={index => setActiveSlide(index)}
+                />
+
+                <View style={styles.paginationContainers}>
+                  <Pagination
+                    dotsLength={data.length}
+                    activeDotIndex={activeSlide}
+                    containerStyle={styles.pagiStyle}
+                    dotColor={'#2E3192'}
+                    dotStyle={styles.pagiDot}
+                    inactiveDotColor={'#EAEAEA'}
+                    inactiveDotOpacity={0.4}
+                    inactiveDotScale={1}
+                    inactiveDotStyle={styles.inactDotStyle}
+                    carouselRef={CarouselRef}
+                    tappableDots={!!CarouselRef}
+                  />
+                </View>
+                <CustomButton
+                  type="theme"
+                  title={'Explore all'}
+                  style={styles.button}
+                  color="#000000"
                 />
               </View>
-              <CustomButton
-                type="theme"
-                title={'Explore all'}
-                style={styles.button}
-                color="#000000"
-              />
             </View>
           </View>
-        </View>
+          <View>
+            <View style={styles.gap} />
 
-        <View style={styles.div}>
-          <View style={styles.txt}>
-            <Text style={styles.p}>Entrance preparation</Text>
+            <View style={styles.div}>
+              <View style={styles.txt}>
+                <Text style={styles.p}>Practice exams</Text>
+              </View>
+              <View style={styles.textContainer}>
+                <Carousel
+                  ref={CarouselReff}
+                  data={data1}
+                  renderItem={_renderItemWithParallax1}
+                  sliderWidth={WIDTH}
+                  itemWidth={WIDTH}
+                  onSnapToItem={index => setActiveSlides(index)}
+                />
+                <View style={styles.paginationContainers}>
+                  <Pagination
+                    dotsLength={data.length}
+                    activeDotIndex={activeSlides}
+                    containerStyle={styles.pagiStyle}
+                    dotColor={'#2E3192'}
+                    dotStyle={styles.pagiDot}
+                    inactiveDotColor={'#EAEAEA'}
+                    inactiveDotOpacity={0.4}
+                    inactiveDotScale={1}
+                    inactiveDotStyle={[
+                      styles.inactDotStyle,
+                      {width: Dimensions.get('window').width * 0.15},
+                    ]}
+                    carouselRef={CarouselReff}
+                    tappableDots={!!CarouselReff}
+                  />
+                </View>
+                <CustomButton
+                  type="theme"
+                  title={'Explore all'}
+                  style={styles.button}
+                  color="#000000"
+                />
+              </View>
+            </View>
+          </View>
+          <View style={styles.gap} />
+
+          <View style={styles.div}>
+            <View>
+              <View style={styles.txt}>
+                <Text style={styles.p}>Entrance preparation</Text>
+              </View>
+
+              <View style={styles.div1}>
+                <View style={styles.textContainer}>
+                  <Carousel
+                    ref={CarouselRefff}
+                    data={data2}
+                    renderItem={_renderItemWithParallax2}
+                    sliderWidth={WIDTH}
+                    itemWidth={WIDTH}
+                    onSnapToItem={index => setActiveSlidess(index)}
+                  />
+                  <View style={styles.paginationContainez}>
+                    <Pagination
+                      dotsLength={data.length}
+                      activeDotIndex={activeSlidess}
+                      containerStyle={styles.pagiStyle}
+                      dotColor={'#2E3192'}
+                      dotStyle={styles.pagiDot}
+                      inactiveDotColor={'#EAEAEA'}
+                      inactiveDotOpacity={0.4}
+                      inactiveDotScale={1}
+                      inactiveDotStyle={styles.inactDotStyle}
+                      carouselRef={CarouselRefff}
+                      tappableDots={!!CarouselRefff}
+                    />
+                  </View>
+                  <CustomButton
+                    type="theme"
+                    title={'Explore all'}
+                    style={styles.button}
+                    color="#000000"
+                  />
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.gap} />
+
+          <View style={styles.div}>
+            <View>
+              <View style={styles.txt}>
+                <Text style={styles.p}>Loksewa preparation</Text>
+              </View>
+
+              <View style={styles.div1}>
+                <View style={styles.textContainer}>
+                  <Carousel
+                    ref={CarouselReffff}
+                    data={data2}
+                    renderItem={_renderItemWithParallax2}
+                    sliderWidth={WIDTH}
+                    itemWidth={WIDTH}
+                    onSnapToItem={index => setActiveSlidesss(index)}
+                  />
+                  <View style={styles.paginationContainez}>
+                    <Pagination
+                      dotsLength={data.length}
+                      activeDotIndex={activeSlidesss}
+                      containerStyle={styles.pagiStyle}
+                      dotColor={'#2E3192'}
+                      dotStyle={styles.pagiDot}
+                      inactiveDotColor={'#EAEAEA'}
+                      inactiveDotOpacity={0.4}
+                      inactiveDotScale={1}
+                      inactiveDotStyle={styles.inactDotStyle}
+                      carouselRef={CarouselReffff}
+                      tappableDots={!!CarouselReffff}
+                    />
+                  </View>
+                  <CustomButton
+                    type="theme"
+                    title={'Explore all'}
+                    style={styles.button}
+                    color="#000000"
+                  />
+                </View>
+              </View>
+            </View>
           </View>
         </View>
       </ScrollView>
