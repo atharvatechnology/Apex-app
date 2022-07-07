3a462b65f8fd29ae3632b3f4b263aee3745f75e4
kundan neupane <kundan.neupane23@gmail.com>
2022-07-07 17:22:15 +0545

    fixed comments again1

--- src/components/templates/ExamDetail.js
+++ src/components/templates/ExamDetail.js
@@ -1,7 +1,7 @@
 /**
  * This is a template for Exam Detailpage. It contains all the modules used in this template.
  * @param {Object} props - contains all the properties of react navigation received from screens.
- * @returns {ExamDetail}- returns a template
+ * @returns {ExamDetailPage }- returns a template
  */
 
 
