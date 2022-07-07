d5698e7f3ce7c4df324a89f1f10931717e4c5ed0
rojesh mote <rojesh123mote@gmail.com>
2022-07-07 12:20:15 +0545

    ExamPayment page components created

--- src/components/modules/Pages/ExamPayment.js
+++ src/components/modules/Pages/ExamPayment.js
@@ -7,7 +7,16 @@
 import React from 'react';
 import {View, Image, TouchableOpacity, Text} from 'react-native';
 
+import CustomButton from '@apexapp/components/elements/CustomButton';
 import styles from '@styles/modules/Pages/ExamPayment';
+
+const data = [
+  {
+    title: 'Instructions',
+    header1: '1.instruction number 1',
+    header2: '2.instruction number 2',
+  },
+];
 
 const ExamPaymentPage = () => {
   return (
@@ -17,7 +26,9 @@ const ExamPaymentPage = () => {
           <Image source={require('@assets/images/leftArrow.png')} />
           <Text style={styles.p}>Payments</Text>
         </TouchableOpacity>
+
         <View style={styles.gap} />
+
         <View style={styles.text}>
           <Text style={styles.baseText}>Test exam 1</Text>
           <Text style={styles.Text}>
@@ -28,10 +39,37 @@ const ExamPaymentPage = () => {
           </View>
         </View>
         <View style={styles.gap} />
+
         <View style={styles.text}>
-          <Text style={styles.baseText}>Instruction</Text>
-          <Text style={styles.Texts}>1.instruction number 1</Text>
-          <Text style={styles.Text}>2.instruction number 2</Text>
+          {data.map((item, index) => {
+            return (
+              <View style={styles.main}>
+                <View style={styles.card}>
+                  <Text style={styles.title}>{item.title}</Text>
+                  <Text style={styles.header1}>{item.header1}</Text>
+                  <Text style={styles.header2}>{item.header2}</Text>
+                </View>
+              </View>
+            );
+          })}
+        </View>
+
+        <View style={styles.footer}>
+          <Image
+            style={styles.image}
+            source={require('@assets/images/esewa.png')}
+          />
+          <View style={styles.txt}>
+            <Text style={styles.fottertext}>Pay with esewa</Text>
+          </View>
+          <View>
+            <CustomButton
+              // type="theme"
+              title={'pay now'}
+              style={styles.button}
+              color="white"
+            />
+          </View>
         </View>
       </View>
     </>
