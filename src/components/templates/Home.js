59a64f385e9663a2366eb61f39316558f0ab6c49
rojesh mote <rojesh123mote@gmail.com>
2022-07-08 12:39:37 +0545

    course overview page created

--- src/components/templates/Home.js
+++ src/components/templates/Home.js
@@ -1,7 +1,7 @@
 /**
  * This is a template for Home screen. It contains all the modules used in this template.
  * @param {Object} props - contains all the propeties of react navigation received from screens.
- * @returns {Home}- returns a template
+ * @returns {Homepage}- returns a template
  */
 
 import React from 'react';
